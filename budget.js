fe8ebf157de6e1008df88edcd3d2dce317dd62db
Paul Nakata <pmn@github.com>
2014-02-12 22:24:43 -0800

    Initial checkin, quick n dirty

---
+++ budget.js
@@ -0,0 +1,32 @@
+function SalesCtrl($scope) {
+    $scope.sales = [
+        {'title': 'Sales Director', 'salary': 177000 },
+        {'title': 'Secretary', 'salary' : 51000 },
+        {'title': 'Mechandising Manager', 'salary': 93200 },
+        {'title': 'Marketing Assistant', 'salary': 55200 },
+        {'title': 'North Sales Manager', 'salary': 90000 },
+        {'title': 'North Salesperson', 'salary': 75000 },
+        {'title': 'North Salesperson', 'salary': 75000 },
+        {'title': 'North Salesperson', 'salary': 75000 },
+        {'title': 'North Salesperson', 'salary': 75000 },
+        {'title': 'South Sales Manager', 'salary': 95000 },
+        {'title': 'South Salesperson', 'salary': 75000 },
+        {'title': 'South Salesperson', 'salary': 75000 },
+        {'title': 'South Salesperson', 'salary': 75000 },
+        {'title': 'South Salesperson', 'salary': 75000 },
+        {'title': 'East Sales Manager', 'salary': 95000 },
+        {'title': 'East Salesperson', 'salary':  75000 },
+        {'title': 'East Salesperson', 'salary':  75000 },
+        {'title': 'East Salesperson', 'salary':  75000 },
+        {'title': 'East Salesperson', 'salary':  75000 },
+        {'title': 'West Sales Manager', 'salary': 95000 },
+        {'title': 'West Sales Person', 'salary': 75000 },
+        {'title': 'West Sales Person', 'salary': 75000 },
+        {'title': 'West Sales Person', 'salary': 75000 },
+        {'title': 'West Sales Person', 'salary': 75000 }];
+};
+
+
+
+var finance = [];
+var logistics = [];
